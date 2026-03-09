78816134acd9a0cab2d0ad23cebb0854127ba6f2
Diane <dianahporter@gmail.com>
2026-03-09 19:12:25 +0200

    feat: Setting the states

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,7 +3,7 @@ function Button({text}){
     return (
     <div className="">
         <button
-    className="w-16 bg-gray-600 text-white rounded-2xl p-4"
+    className="w-16 bg-gray-600 text-white rounded-2xl p-4 hover:bg-orange-400"
     >{text}</button>
     </div>
     );
--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Button from './Button'
 function Calculator(){
       const buttons = [
@@ -5,12 +6,38 @@ function Calculator(){
                     '7', '8', '9', 'X',
                     '4', '5', '6', '-',
                     '1', '2', '3', '+',
-                    '0', '.', '=', '='
+                    '0', '.', '='
                     ];
+    const [display, setDisplay] = useState('0');
+    const [previous, setPrevious] = useState(null);
+    const [operator, setOperator] = useState(null);
+
+    function handleNumber(num){
+        if(display === '0'){
+            setDisplay(num);
+        }
+        else{
+            setDisplay( display + num);
+        }
+    }
+
+    function handleOperator(op){
+        setPrevious(display);
+        setOperator(op);
+        setDisplay('0')
+
+    }
+
+    function handleClick(value){
+        if(!isNaN(value) || value == 0){
+            
+        }
+    }
+
     return (
         <div className='w-75 border-2 mx-auto my-3 bg-gray-950 p-3 rounded-2xl'>
             <h1 className=" text-2xl py-4 text-white  ">My Calculator App</h1>
-            <input type="text"
+            <input type="text" 
             className=" bg-gray-300 w-68 p-4 text-2xl "
             placeholder={'0'}
             />
