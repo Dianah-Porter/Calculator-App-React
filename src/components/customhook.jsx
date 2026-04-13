b4ff650d4e0270de8b61f9261551e48a1813a25f
Diane <dianahporter@gmail.com>
2026-04-13 10:05:23 +0200

    feat: Custom hook for useGetById

--- src/components/customhook.jsx
+++ src/components/customhook.jsx
@@ -2,31 +2,14 @@ import { useEffect, useState } from "react"
 
 const useGetUserById = (userId) =>{
     const [user, setUser] = useState(null);
-    const [posts, setPosts] = useState(null);
-    const [loading, setLoading] = useState();
-    const [error, setError] = useState();
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     
-    useEffect(async ()=>{
-        try{
-            let user = await fetch('https://jsonplaceholder.typicode.com/users');
-            let posts = await fetch('https://jsonplaceholder.typicode.com/posts');
-            let userjson = await user.json();
-            let userdata = userjson.data;
-            let postsjson = await posts.json();
-            let postsdata = postsjson.data;
-
-            setUser(userdata);
-            setPosts(postsdata);
-        }
-        catch{
-            setError(error);
-        }
-        finally{
+    useEffect(()=> {
+        let fetching = async () => {
 
         }
-    },[user, posts])
-
-    return {user, posts, loading, error}
-    
+    })
 }
 export default useGetUserById;
