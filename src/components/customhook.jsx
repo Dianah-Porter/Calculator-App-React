ba1c37ab6a033995d542f5a9189091c28f77b044
Diane <dianahporter@gmail.com>
2026-04-13 10:43:55 +0200

    feat: fetching data

--- src/components/customhook.jsx
+++ src/components/customhook.jsx
@@ -7,9 +7,25 @@ const useGetUserById = (userId) =>{
     const [error, setError] = useState(null);
     
     useEffect(()=> {
-        let fetching = async () => {
+        let fetchData = async () => {
+            if(!userId) return;
 
+            try{
+                setLoading(true);
+                setError(null);
+
+                const [userRes, postRes] = await Promise.all([
+                            fetch('https://jsonplaceholder.typicode.com/users'),
+                            fetch('https://jsonplaceholder.typicode.com/posts')
+                        ]);
+                if(!userRes.ok || !postRes.ok) throw new Error("")
+            }
+            catch(err){
+                setError(err);
+            }
+            
         }
-    })
-}
+    });
+    return {user, posts, loading, error};
+};
 export default useGetUserById;
